ad3a2ccca6de1c5d3608a8c648260dde03532c91
harish242 <markollh242@gmail.com>
2023-06-09 13:49:11 +0530

    gfhg

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import '../styles/App.css';
 const App = () => {
-  
+  const [currentNumber,setCurrentNumber]=useState(1)
+  const numbersPerpage=5;
+  const getNumberarr=()=>{
+    const arr=[]
+    for(let i=(currentNumber-1)*numbersPerpage+1;i<=numbersPerpage*currentNumber;i++){
+      arr.push(<div>{i}</div>)
+    }
+    // for (let i = (currentPage - 1) * numbersPerPage + 1; i <= currentPage * numbersPerPage; i++) {
+    //   numberDisplay.push(<div key={i}>{i}</div>);
+    // }
+    return arr
+  }
+  const HandleNext=()=>{
+    setCurrentNumber((prev)=>prev+1)
+  }
+  const HandlePrev=()=>{
+    setCurrentNumber((prev)=>prev-1)
+  }
+  const prevbtn=currentNumber===1
+
 
   return (
     <div id="main">
 
       <div>
-
+      {getNumberarr()}
       </div>
+      <button id='back-button' disabled={prevbtn} onClick={HandlePrev}>Prev Page</button>
+      <button id='next-button' onClick={HandleNext}>Next Page</button>
     </div>
   )
 }
